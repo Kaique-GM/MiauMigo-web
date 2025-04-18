42fd4326bf9ca6190fab9fa4b4677ffb36ddeed0
Kaique-GM <kaiquegmoficial@gmail.com>
2025-04-17 22:05:01 -0300

    Footer + Header foi corrigido a posição na tela

--- MiauMigo/src/app/footer/footer.component.ts
+++ MiauMigo/src/app/footer/footer.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  email: string = '';
 
+  @ViewChild('form') form!: NgForm;
+
+  enviarNewsletter() {
+
+    if (this.form.valid) {
+      alert(`Novidades serão enviadas para: ${this.email}`);
+      this.email = '';
+    }
+  }
 }
