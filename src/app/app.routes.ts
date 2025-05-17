0ce9d42c8af81fead5d7060e68b7be5c7851b457
Kaique-GM <kaiquegmoficial@gmail.com>
2025-05-16 22:26:29 -0300

    Tela Login 80%

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,26 +6,42 @@ import { AnunciarComponent } from './anunciar/anunciar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LoginComponent } from './login/login.component';
-import { OpcaoCadastroComponent } from './opcao-cadastro/opcao-cadastro.component';
 import { CadastroVendedorComponent } from './cadastro-vendedor/cadastro-vendedor.component';
 import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 export const routes: Routes = [
 
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'shop', component: ShopComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'anunciar', component: AnunciarComponent },
-    { path: 'product-detail/:id', component: ProductDetailComponent },
+
+    // Main layout
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'shop', component: ShopComponent },
+            { path: 'about', component: AboutComponent },
+            { path: 'anunciar', component: AnunciarComponent },
+            { path: 'product-detail/:id', component: ProductDetailComponent },
 
 
-    { path: 'opcao-cadastro', component: OpcaoCadastroComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'cadastro-cliente', component: CadastroClienteComponent },
-    { path: 'cadastro-vendedor', component: CadastroVendedorComponent },
+        ],
+    },
 
+    // Login layout
+    {
+        path: '',
+        component: AuthLayoutComponent,
+        children: [
+            { path: 'login', component: LoginComponent },
+            { path: 'cadastro-cliente', component: CadastroClienteComponent },
+            { path: 'cadastro-vendedor', component: CadastroVendedorComponent },
 
+        ],
+    },
 
     { path: '**', component: NotFoundComponent }
+
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink,RouterLinkActive],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  perfilSelecionado: 'cliente' | 'vendedor' | null = null;
+  gatinho: boolean = true;
+
+  email: string = "";
+  senha: string = "";
+
+  @ViewChild('form') form!: NgForm;
+
+  selecionarPerfil(perfil: 'cliente' | 'vendedor') {
+    this.perfilSelecionado = perfil;
+  }
+
+  login(usuario: string) {
+    this.gatinho = false;
+  }
+
+  voltar() {
+    this.perfilSelecionado = null;
+  }
 }
