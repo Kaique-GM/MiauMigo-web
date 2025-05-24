c47a2547e093c2d1ab228b8bcd6d131a0bde537f
Kaique-GM <kaiquegmoficial@gmail.com>
2025-05-23 22:27:52 -0300

    Processo de Login 95%

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive, FormsModule],
+  imports: [RouterLink, RouterLinkActive, FormsModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   constructor(private router: Router) { }
+  ngOnInit(): void {
+    const usuarioSalvo = localStorage.getItem('user');
+    if (usuarioSalvo !== null) {
+      const usuario = JSON.parse(usuarioSalvo);
 
+      this.session = true;
+      this.tipo = usuario.tipo;
+
+      if (usuario.Image !== '') {
+        this.img = "/Login/" + usuario.Image;
+      }
+    }
+
+  }
+
+  session: boolean = false;
   filter: string = '';
+  img: string = '';
+  tipo: string = '';
+  showDropdown: boolean = false;
+  headerClass = '';
+
+  onUserImgEnter() {
+    this.headerClass = 'user-hover';
+    this.showDropdown = true;
+  }
+
+  onUserImgLeave() {
+    this.showDropdown = false;
+    this.headerClass = '';
+  }
 
   home() {
     this.router.navigate(['/home']);
@@ -25,8 +55,12 @@ export class HeaderComponent {
     } else {
       this.router.navigate(['/shop'], { queryParams: { search: this.filter } });
     }
-    
+
     this.filter = '';
+  }
+
+  logout() {
+    this.session = false;
   }
 
 }
